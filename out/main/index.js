b25cfcdd90e6fe5f435eff094627571d9ed3ba82
Jacob Estep <jakeste.17@gmail.com>
2025-01-18 08:32:02 -0700

    unsure

---
+++ out/main/index.js
@@ -0,0 +1,53 @@
+"use strict";
+const electron = require("electron");
+const path = require("path");
+const utils = require("@electron-toolkit/utils");
+const icon = path.join(__dirname, "./chunks/icon-e8a87faa.png");
+function createWindow() {
+  const mainWindow = new electron.BrowserWindow({
+    width: 350,
+    height: 300,
+    minWidth: 10,
+    minHeight: 10,
+    show: false,
+    alwaysOnTop: true,
+    autoHideMenuBar: true,
+    transparent: true,
+    frame: false,
+    icon,
+    webPreferences: {
+      preload: path.join(__dirname, "../preload/index.js"),
+      sandbox: false
+    }
+  });
+  mainWindow.on("ready-to-show", () => {
+    mainWindow.show();
+  });
+  mainWindow.webContents.setWindowOpenHandler((details) => {
+    electron.shell.openExternal(details.url);
+    return { action: "deny" };
+  });
+  if (utils.is.dev && process.env["ELECTRON_RENDERER_URL"]) {
+    mainWindow.loadURL(process.env["ELECTRON_RENDERER_URL"]);
+  } else {
+    mainWindow.loadFile(path.join(__dirname, "../renderer/index.html"));
+  }
+}
+if (require("electron-squirrel-startup"))
+  electron.app.quit();
+electron.app.whenReady().then(() => {
+  utils.electronApp.setAppUserModelId("com.electron");
+  electron.app.on("browser-window-created", (_, window) => {
+    utils.optimizer.watchWindowShortcuts(window);
+  });
+  createWindow();
+  electron.app.on("activate", function() {
+    if (electron.BrowserWindow.getAllWindows().length === 0)
+      createWindow();
+  });
+});
+electron.app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    electron.app.quit();
+  }
+});
